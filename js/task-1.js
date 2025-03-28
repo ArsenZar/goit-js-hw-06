0db98f12ddc5f53cbfe17c8f141a4fe603d82260
ArsenZar <arsenzar@gmail.com>
2025-03-28 13:35:08 +0200

    practice-2 (context)

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,25 @@
+/*practice-2 (context - how to use object for give function context )*/
+/*
+"use strict";
+
+function showThis() {
+  console.log("this in showThis: ", this);
+}
+
+const user = {
+  username: "Poly",
+};
+
+user.showContext = showThis;
+
+// Викликаємо в контексті об'єкта
+user.showContext(); // this in showThis: {username: "Poly", showContext: ƒ}
+
+// Викликаємо в глобальному контексті
+showThis(); // "this in showThis: undefined"
+
+*/
+
 /*practice-1 (this)*/
 /*
 const user = {
