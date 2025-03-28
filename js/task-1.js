f6218448f3b0bb15763a188334414c0219aa4b6e
ArsenZar <arsenzar@gmail.com>
2025-03-28 13:24:41 +0200

    practice-1 (this)

---
+++ js/task-1.js
@@ -0,0 +1,24 @@
+/*practice-1 (this)*/
+/*
+const user = {
+  username: "Victor",
+  showName() {
+		// ❌ Віктор біжить швидко, тому що Віктор намагається зловити поїзд
+    console.log(user.username);
+  },
+};
+
+user.showName();
+
+
+const user = {
+  username: "Victor",
+  showName() {
+		// ✅ Віктор біжить швидко, тому що він (this) намагається зловити поїзд.
+    console.log(this.username);
+  },
+};
+
+user.showName();
+
+ */
