54e9fd743cb5bb567c412f68871c3ee208a8235f
ArsenZar <arsenzar@gmail.com>
2025-03-29 21:37:38 +0200

    practice-14 (privet mathods)

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,54 @@
+
+
+/* practice-14 (privet methods) */
+
+class User {
+  name;
+  #email;
+
+  constructor(params) {
+    this.name = params.name;
+    this.#email = params.email;
+  }
+
+  // Публічний метод для отримання електронної пошти
+  getEmail() {
+    return this.#email;
+  }
+
+  // Публічний метод для зміни електронної пошти
+  changeEmail(newEmail) {
+    if (this.#validateEmail(newEmail)) {
+      this.#email = newEmail;
+    } else {
+      console.log('Invalid email format');
+    }
+  }
+
+  // Приватний метод для валідації електронної пошти
+  #validateEmail(email) {
+    return email.includes('@');
+  }
+}
+
+const mango = new User({
+  name: 'Mango',
+  email: 'mango@mail.com',
+});
+
+// Спробуємо змінити електронну пошту
+mango.changeEmail('newmail.com'); // "Invalid email format"
+mango.changeEmail('new@mail.com');
+console.log(mango.getEmail()); // "new@mail.com"
+
+// Прямий виклик приватного методу ззовні призведе до помилки
+mango.#validateEmail('test'); // Помилка
+
+
+//////////////////////////////////////////////////////////////////////////
+
 /* practice-13.1 (how to need work with privet data) */
+/*
 class User {
   name;
   #email;
@@ -25,6 +75,7 @@ const mango = new User({
 console.log(mango.getEmail()); // "mango@mail.com"
 mango.changeEmail("mango@supermail.com");
 console.log(mango.getEmail()); // "mango@supermail.com"
+*/
 
 //////////////////////////////////////////////////////////////////////////
 
