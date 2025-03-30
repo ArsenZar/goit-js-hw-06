2f90fc500866c04506307bc6d287a7e0b650e438
ArsenZar <arsenzar@gmail.com>
2025-03-31 00:21:29 +0300

    task-1 ready

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,38 @@
-/* practice-18  */
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
 
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+
+
+/* practice-18  */
+/*
 class User {
   email;
 
@@ -51,7 +84,7 @@ mango.blacklist("poly@mail.com");
 console.log(mango.blacklistedEmails); // ["poly@mail.com"]
 console.log(mango.isBlacklisted("mango@mail.com")); // false
 console.log(mango.isBlacklisted("poly@mail.com")); // true
-
+*/
 
 //////////////////////////////////////////////////////////////////////////
 
